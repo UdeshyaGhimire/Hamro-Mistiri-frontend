7a6dae3fd7a1cf375aa1ba5e2c9fba7f11a2c540
Tracyshrestha <tracyyyshrestha@gmail.com>
2022-10-07 21:22:25 +0545

    add User and Mistiri Login and Registration pages

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router , Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from "./Navbar";
 import NotFound from "./NotFound";
 import HomePage from "./pages/HomePage";
-import Service from "./pages/Service";
 import LogInUser from "./pages/LogInUser";
 import LogInMistiri from "./pages/LogInMistiri";
 import RegisterUser from "./pages/RegisterUser";
 import RegisterMistiri from "./pages/RegisterMistiri";
 
-import Plumber from "./pages/Plumber";
-
-
-
 function Main() {
   return (
     <Router>
-    <div >
-        <Navbar/>  
+      <div >
+        <Navbar />
         {/* calling Navbar for every page. the component below this navbar will only be changed when we change the pages! */}
-          <main >
-              <Routes>
-              <Route exact path="/" element={<HomePage/>}/>
-              <Route   path="/service" element={<Service/>}/>
-
-              <Route   path="/loginuser" element={<LogInUser/>}/>
-              <Route   path="/loginmistiri" element={<LogInMistiri/>}/>
-              <Route   path="/registeruser" element={<RegisterUser/>}/>
-              <Route   path="/registermistiri" element={<RegisterMistiri/>}/>
-            
-              <Route   path="/plumber" element={<Plumber/>}/>
-
-              <Route path="*" element={<NotFound/>}/>
-              </Routes> 
-          </main>
-    </div>
+        <main >
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/loginuser" element={<LogInUser />} />
+            <Route path="/loginmistiri" element={<LogInMistiri />} />
+            <Route path="/registeruser" element={<RegisterUser />} />
+            <Route path="/registermistiri" element={<RegisterMistiri />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
