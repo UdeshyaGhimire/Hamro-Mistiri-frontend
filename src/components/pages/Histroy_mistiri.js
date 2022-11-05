2ffec0affb193b13b5a128de224bd0bad057b236
Anurag Baskota <55234656+Anuragh6911@users.noreply.github.com>
2022-11-05 19:27:27 +0545

    complete user history page

--- src/components/pages/Histroy_mistiri.js
+++ src/components/pages/Histroy_mistiri.js
@@ -2,63 +2,86 @@ import React from 'react';
 import '../../css/Histroy_mistiri.css';
 import axios from 'axios';
 import DashboardComM from "./DashboardComM";
+import { useParams } from 'react-router-dom';
 
 
 
 
 function Histroy_mistiri() {
 
-    return (
-
-<div className=' md:grid grid-cols-5 w-full h-full '>
-
-
-<DashboardComM />
+  const params = useParams();
+  const userId = params.id;
 
 
-{/* Edit Form Starts Here */}
-<div className='col-span-4'>
-
-<div class="table">
-<table>
-  <caption>Mistiri Histroy Page</caption>
-  <caption>Hired By:</caption>
-
-  <thead>
-    <tr>
-      <th scope="col">Problem ID</th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Email</th>
-
-      <th scope="col">Description</th>
-
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td data-label="Problem ID">112</td>
-      <td data-label="First Name">Ram</td>
-      <td data-label="Last Name">Shah</td>
-      <td data-label="Phone">9864241171</td>
-      <td data-label="Email">shahram@gmail.com</td>
-      <td data-label="Description">pipe leakage</td>
+  const [mistiriHistory, setMistiriHistory] = React.useState([]);
 
 
-    </tr>
-    
-  </tbody>
-</table>
+  React.useEffect(() => {
+    console.log("hello inside useeEffect");
+    const url1 = `http://localhost:8080/mistiriHistory/${userId}`;
+    axios.get(url1)
+      .then((res) => {
+        // console.log(res.data);
+        console.log(res.data);
+        setMistiriHistory(res.data);
+
+      })
+      .catch(err => console.log(err));
+  }, []);
 
 
-</div>
+  return (
 
-</div>
+    <div className=' md:grid grid-cols-5 w-full h-full '>
+
+
+      <DashboardComM />
+
+
+      {/* Edit Form Starts Here */}
+      <div className='col-span-4'>
+
+        <div class="table">
+          <table>
+            <caption>Mistiri Histroy Page</caption>
+            <caption>Hired By:</caption>
+
+            <thead>
+              <tr>
+                <th scope="col">Problem ID</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Email</th>
+
+                <th scope="col">Description</th>
+
+              </tr>
+            </thead>
+            <tbody>
+            {mistiriHistory.map(problem => (
+              <tr>
+                <td data-label="Problem ID">{problem.id}</td>
+                <td data-label="First Name">{problem.customer?.firstName}</td>
+                <td data-label="Last Name">{problem.customer?.lastName}</td>
+                <td data-label="Phone">{problem.customer?.phoneNo}</td>
+                <td data-label="Email">{problem.customer?.email}</td>
+                <td data-label="Description">{problem.description}</td>
+
+
+              </tr>
+              ))}
+            </tbody>
+          </table>
+
+
+        </div>
+
+      </div>
 
 
 
-</div>
-    );
+    </div>
+  );
 }
 export default Histroy_mistiri;
--- src/components/pages/Histroy_user.js
+++ src/components/pages/Histroy_user.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import '../../css/Histroy_user.css';
 import axios from 'axios';
-import DashboardComM from "./DashboardComM";
+import DashboardComU from "./DashboardComU";
+import { useParams } from 'react-router-dom';
 
 
 
 
 function Histroy_user() {
 
+  const params = useParams();
+  const userId = params.id;
+
+
+  const [userHistory, setUserHistory] = React.useState([]);
+
+
+  React.useEffect(() => {
+    console.log("hello inside useeEffect");
+    const url1 = `http://localhost:8080/customerHistory/${userId}`;
+    axios.get(url1)
+      .then((res) => {
+        // console.log(res.data);
+        console.log(res.data);
+        setUserHistory(res.data);
+
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+
+
     return (
 
 <div className=' md:grid grid-cols-5 w-full h-full '>
 
 
-<DashboardComM />
+<DashboardComU />
 
 
 {/* Edit Form Starts Here */}
@@ -38,18 +61,19 @@ function Histroy_user() {
     </tr>
   </thead>
   <tbody>
+  {userHistory.map(problem => (
     <tr>
-    <td data-label="Problem ID">112</td>
-      <td data-label="First Name">Ram</td>
-      <td data-label="Last Name">Shah</td>
-      <td data-label="Phone">9864241171</td>
-      <td data-label="Email">shahram@gmail.com</td>
-      <td data-label="Description">pipe leakage</td>
-      <td data-label="Description">pipe leakage in my house is very much</td>
+    <td data-label="Problem ID">{problem.id}</td>
+      <td data-label="First Name">{problem.mistiriDetail?.customer?.firstName}</td>
+      <td data-label="Last Name">{problem.mistiriDetail?.customer?.lastName}</td>
+      <td data-label="Phone">{problem.mistiriDetail?.customer?.phoneNo}</td>
+      <td data-label="Email">{problem.mistiriDetail?.customer?.email}</td>
+      <td data-label="Description">{problem.mistiriDetail?.service}</td>
+      <td data-label="Description">{problem.description}</td>
 
       
     </tr>
-    
+    ))}
   </tbody>
 </table>
 
